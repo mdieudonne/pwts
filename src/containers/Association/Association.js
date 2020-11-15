d4114480db71a28dbc9de2d970b78656ff23f25e
mdieudonne <michael.dieudonne@outlook.com>
2020-11-15 22:37:38 +0100

    feat: add Association chore: add Meta tags

--- src/containers/Association/Association.js
+++ src/containers/Association/Association.js
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react';
 import useTheme from "@material-ui/core/styles/useTheme";
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -40,25 +39,25 @@ function Association(props) {
           justify="center"
           alignItems="center">
       <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={theme.palette.type === 'dark' ? logo : logoLight}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Association
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Progressive Wing Tsun System Rhône Alpes propose des cours de Wing Tsun / Wing Chun Kung fu à Lyon.
-              PWTSRA ce sont des techniques d'apprentissage modernes avec des cours structurés et des programmes adaptés
-              au niveau des élèves. En savoir plus
-            </Typography>
-          </CardContent>
-        </CardActionArea>
+        {/*<CardActionArea>*/}
+        <CardMedia
+          className={classes.media}
+          image={theme.palette.type === 'dark' ? logo : logoLight}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Association
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Progressive Wing Tsun System Rhône Alpes propose des cours de Wing Tsun / Wing Chun Kung fu à Lyon.
+            PWTSRA ce sont des techniques d'apprentissage modernes avec des cours structurés et des programmes adaptés
+            au niveau des élèves. En savoir plus
+          </Typography>
+        </CardContent>
+        {/*</CardActionArea>*/}
         <CardActions>
-          <a href="https://www.pwts.fr/" target="_blank">
+          <a href="https://www.pwts.fr/" target="_blank" style={{textDecoration: 'none'}}>
             <Button size="small" color="primary">
               En savoir plus !
             </Button>
